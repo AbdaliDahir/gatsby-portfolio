b357f53a3fce654135bb56a8e3ee5d672d6b8edd
AbdaliDahir <abdali.dahir@gmail.com>
2020-08-10 00:46:31 +0200

    return style

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,7 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./navbar"
 import Footer from "./footer"
 
-import { ContainerLayout, MainContent} from '../common';
+import {GlobalStyle, ContainerLayout, MainContent} from '../common';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -18,7 +18,8 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <> 
+    <>
+      <GlobalStyle />
       <MainContent>
         <ContainerLayout>
           <Navbar siteTitle={data.site.siteMetadata.title} />

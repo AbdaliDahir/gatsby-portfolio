8b6c5c968dc4919f89504ad058ee583433832fc5
aharri64 <haveacompleteday@gmail.com>
2021-04-17 15:09:34 -0600

    set up and ready to go

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import {AboutSection, Avatar, Title, Text, SubTitle} from './style';
-import {SectionIntro, ContainerLayout, ResumeButton} from "../common";
+import { AboutSection, Avatar, Title, Text, SubTitle } from './style';
+import { SectionIntro, ContainerLayout, ResumeButton } from "../common";
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -22,11 +22,11 @@ const About = () => {
           <AboutSection>
             <div>
               <Avatar fluid={data.placeholderImage.childImageSharp.fluid} alt="user photo" />
-              <SubTitle> Front End Developer</SubTitle>
-            </div> 
+              <SubTitle>Full Stack Developer</SubTitle>
+            </div>
             <div>
-              <Title> Hello, I’m AbdAli </Title>
-              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">North Africa</b> living in Casablanca. </Text>
+              <Title> Hello, I’m Amir </Title>
+              <Text> I'm a full stack developer from <b className="text-primary lined-link">Denver, Colorado</b></Text>
               <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
               <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
               </Text>

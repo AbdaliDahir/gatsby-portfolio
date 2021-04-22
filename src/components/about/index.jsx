837f8ef3b129e84b66b84bfa35a95a59d085b3b6
aharri64 <haveacompleteday@gmail.com>
2021-04-21 18:57:02 -0600

    added functionality

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { AboutSection, Avatar, Title, Text, SubTitle } from './style';
 import { SectionIntro, ContainerLayout, ResumeButton } from "../common";
+import DevIcon from "devicon-react-svg";
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -22,14 +23,95 @@ const About = () => {
           <AboutSection>
             <div>
               <Avatar fluid={data.placeholderImage.childImageSharp.fluid} alt="user photo" />
-              <SubTitle>Full Stack Developer</SubTitle>
+              <SubTitle>Full-Stack Developer</SubTitle>
+              <div class="devicon" width="50px">
+                <div className="content_img">
+                  <DevIcon icon="react" width="40" />
+                  <div>React</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="nodejs" width="40" />
+                  <div>Node.js</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="javascript" width="40" />
+                  <div>JavaScript</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="python" width="40" />
+                  <div>Python</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="django" width="40" />
+                  <div>Django</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="mongodb" width="40" />
+                  <div>MongoDB</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="postgresql" width="40" />
+                  <div>PostgreSQL</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="git" width="40" />
+                  <div>Git</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="jira" width="40" />
+                  <div>Jira</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="markdown" width="40" />
+                  <div>Markdown</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="jquery" width="40" />
+                  <div>jQuery</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="html5" width="40" />
+                  <div>HTML5</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="css3" width="40" />
+                  <div>CSS3</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="sass" width="40" />
+                  <div>Sass</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="bootstrap" width="40" />
+                  <div>Bootstrap</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="npm" width="40" />
+                  <div>NPM</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="visualstudio" width="40" />
+                  <div>Visual Studio Code</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="heroku" width="40" />
+                  <div>Heroku</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="illustrator" width="40" />
+                  <div>Adobe Illustrator</div>
+                </div>
+                <div className="content_img">
+                  <DevIcon icon="photoshop" width="40" />
+                  <div>Adobe Photoshop</div>
+                </div>
+              </div>
             </div>
             <div>
               <Title> Hello, I’m Amir </Title>
-              <Text> I'm a full stack developer from <b className="text-primary lined-link">Denver, Colorado</b></Text>
-              <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
-              <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
-              </Text>
+              <Text>I'm a former Sommelier and US Marine turned software developer from <b className="text-primary lined-link">Denver, Colorado</b></Text>
+              <Text> I love working with modern technologies, as well as building and designing awesome projects. I try to use every opportunity to challenge both hemispheres of my brain and I'm always in a consistent state of self improvement. </Text>
+              <Text>I'm a highly positive, focused, and organized person, I bring with me the immense potential to handle challenges and opportunities equally with undying enthusiasm and growth mindset. I am easy to work with, adapt quickly in a team environment, and good at anticipating the needs and concerns in any team collaboration.</Text>
               <ResumeButton href="resume_amir_harrison.pdf" target="_blank"> Download resume </ResumeButton>
             </div>
           </AboutSection>

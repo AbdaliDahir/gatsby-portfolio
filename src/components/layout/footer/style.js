7d535d442f660d273d0bc3be9126fb6f72a1a6a7
aharri64 <haveacompleteday@gmail.com>
2021-04-21 18:57:30 -0600

    added style

--- src/components/layout/footer/style.js
+++ src/components/layout/footer/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import variables from '../../../data/variables';
 
+// this is the quote
 export const FooterStyle = styled.footer`
 	padding-top: 1rem;
   padding-bottom: 8rem;
@@ -17,7 +18,7 @@ export const FooterStyle = styled.footer`
 		text-align: center;
 	}
 	.quote {
-		font-size: 4rem;
+		font-size: 2rem;
 		font-family: "GT-Walsheim-Pro-Bold";
 		line-height: 1.2;
 		color: ${variables.primary};
@@ -31,27 +32,32 @@ export const SubRight = styled.p`
 	font-size: 1rem;
 	line-height: 1.8;
 	letter-spacing:2px;
-	text-transform: capitalize;
+	// text-transform: capitalize;
 	margin-bottom: 20px;
 	font-family: "GT-Walsheim-Pro-Bold";
-`	
+	
+`
 export const CopyRight = styled.p`
-	font-size: 11px;
+	font-size: 16px;
 	line-height: 1.8;
 	letter-spacing:1px;
-	text-transform: capitalize;
+	// text-transform: capitalize;
 `
+//left
 export const FooterBody = styled.div`
+	color: yellow;
 	display: grid;
 	grid-template-columns: 1fr 1fr;
-	align-items: center;
+	align-items: flex-start;
 	@media(max-width: ${variables.breakpointPhone}) {
 		grid-template-columns: 1fr;
 	}
+	
 `
 export const FooterSocialMedia = styled.ul`
 	list-style: none;
 	padding-left: 0;
+	
 	@media(max-width: ${variables.breakpointPhone}) {
 		display: flex;
 		align-items: center;
@@ -65,9 +71,9 @@ export const MediaLink = styled.a`
 	padding: 1rem 0rem;
 	padding-bottom: .5rem;
 	line-height: 1;
-	font-size: 1rem;
-  text-transform: capitalize;
-  font-family: "GT-Walsheim-Pro-Bold";
-	color: ${variables.primary};
+	font-size: 40px;
+  	text-transform: capitalize;
+  	font-family: "GT-Walsheim-Pro-Bold";
+	color: green;
 	
 `
